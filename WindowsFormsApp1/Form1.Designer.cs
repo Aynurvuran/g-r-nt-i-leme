30c57c5af286ba8b5372a743b8987151211292cd
elvaneryilmaz <43929986+elvaneryilmaz@users.noreply.github.com>
2018-10-08 23:55:23 +0300

    imageproc.ödev

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.işlemlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renkgetirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.griYöntemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.işlemlerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kapatToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // kapatToolStripMenuItem
+            // 
+            this.kapatToolStripMenuItem.Name = "kapatToolStripMenuItem";
+            this.kapatToolStripMenuItem.Size = new System.Drawing.Size(123, 26);
+            this.kapatToolStripMenuItem.Text = "Kapat";
+            this.kapatToolStripMenuItem.Click += new System.EventHandler(this.kapatToolStripMenuItem_Click);
+            // 
+            // işlemlerToolStripMenuItem
+            // 
+            this.işlemlerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renkgetirToolStripMenuItem,
+            this.griYöntemToolStripMenuItem});
+            this.işlemlerToolStripMenuItem.Name = "işlemlerToolStripMenuItem";
+            this.işlemlerToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.işlemlerToolStripMenuItem.Text = "İşlemler";
+            // 
+            // renkgetirToolStripMenuItem
+            // 
+            this.renkgetirToolStripMenuItem.Name = "renkgetirToolStripMenuItem";
+            this.renkgetirToolStripMenuItem.Size = new System.Drawing.Size(157, 26);
+            this.renkgetirToolStripMenuItem.Text = "Renkgetir";
+            this.renkgetirToolStripMenuItem.Click += new System.EventHandler(this.renkgetirToolStripMenuItem_Click);
+            // 
+            // griYöntemToolStripMenuItem
+            // 
+            this.griYöntemToolStripMenuItem.Name = "griYöntemToolStripMenuItem";
+            this.griYöntemToolStripMenuItem.Size = new System.Drawing.Size(157, 26);
+            this.griYöntemToolStripMenuItem.Text = "Gri Yöntem";
+            this.griYöntemToolStripMenuItem.Click += new System.EventHandler(this.griYöntemToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kapatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem işlemlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renkgetirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem griYöntemToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        Bitmap kaynak;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = openFileDialog1.ShowDialog();
+
+            if (sonuc == DialogResult.OK)
+
+            {
+
+                kaynak = new Bitmap(openFileDialog1.FileName);
+
+                kaynakBox.Image = kaynak;
+
+            }
+        }
+
+        private void renkgetirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(xkor.Text);
+
+            int y = int.Parse(ykor.Text);
+
+
+
+            Color rengim = kaynak.GetPixel(x, y);
+
+            islemBox.BackColor = rengim;
+
+            Console.WriteLine("R: " + rengim.R + " G: " + rengim.G + " B: " + rengim.B);
+        }
+    }
+}
